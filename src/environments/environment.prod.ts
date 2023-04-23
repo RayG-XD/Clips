b85acb209995291d76b2b38d1b84f03ac07c3179
Pratik Pol <pratikpol0602@gmail.com>
2023-04-23 14:18:02 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD8uFzIoTviNFp7y1_JIXt_nmdKGTthft0",
+    authDomain: "clips-407a4.firebaseapp.com",
+    projectId: "clips-407a4",
+    storageBucket: "clips-407a4.appspot.com",
+    messagingSenderId: "789707291124",
+    appId: "1:789707291124:web:a10ece250e3b740a63e1a1"
+  }
+};
